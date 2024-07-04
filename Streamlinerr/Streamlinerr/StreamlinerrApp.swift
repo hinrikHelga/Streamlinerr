5afcd9747e582109741eca638c94dce6d94c0817
Hinrik Helgason <hihe@trifork.com>
2024-07-04 13:39:13 +0200

    Init project

---
+++ Streamlinerr/Streamlinerr/StreamlinerrApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamlinerrApp.swift
+//  Streamlinerr
+//
+//  Created by Hinrik Helgason on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct StreamlinerrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
